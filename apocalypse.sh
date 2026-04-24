97b11e2e50458794c0e9ab80e8318eb40cbfad36
jefripunza <jefripunza@gmail.com>
2026-04-24 10:17:13 +0700

    feat: add confirmation prompt before executing project destruction script

--- apocalypse.sh
+++ apocalypse.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+read -p "Apakah Anda yakin ingin menghancurkan project ini? [y/N] " confirm
+if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
+  echo "Penghancuran project dibatalkan."
+  exit 0
+fi
+
 rm -rf *
 rm -rf .agent .air.toml .dockerignore .git .github .gitignore
 
